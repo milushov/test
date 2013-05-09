ba24160da18114f605d65368af765f55061cea31
Roma Milushov <roma@milushov.ru>
2013-05-09 18:13:27 +0400

    fix

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,4 @@
 class Comment < ActiveRecord::Base
   # attr_accessible :title, :body
-  belongs_to :post, foreign_key: 'post_test_id'
+  belongs_to :post, primary_key: 'post_test_id', foreign_key: 'post_test_id'
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ActiveRecord::Base
   attr_accessible :body, :test_id
-  has_many :comments, primary_key: 'post_test_id', foreign_key: 'post_test_id'
+  has_many :comments, primary_key: 'test_id'
 end
